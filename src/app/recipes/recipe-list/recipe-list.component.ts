b51b5fc7bac476ec4a965d7e92003d15ff7d5786
Arun Rajagopalan <arunmozhivarmanr@gmail.com>
2019-01-25 23:48:21 +0530

    Recipe list added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
- 
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -8,8 +8,10 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is the test recipe', 
-    'https://c.pxhere.com/photos/8b/0f/food_meat_recipe_power_pork_dishes-604134.jpg!d')
+    new Recipe('A Test Recipe', 'This is the test recipe',
+    'https://c.pxhere.com/photos/8b/0f/food_meat_recipe_power_pork_dishes-604134.jpg!d'),
+    new Recipe('A Test Recipe1', 'This is the test recipe1',
+    'https://images-gmi-pmc.edge-generalmills.com/d7812377-2602-4222-b78c-c0904cd1fd3b.jpg')
   ];
 
   constructor() { }
