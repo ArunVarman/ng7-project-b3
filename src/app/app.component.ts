0fe36369750a589a25031d44da3b94674bd711dc
Arun Rajagopalan <arunmozhivarmanr@gmail.com>
2019-02-03 23:02:08 +0530

    Lazy loading added for Recipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   loadedFeature = 'recipe';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyD8UxOS7l0W3I7Ha5KWaa-nL5xjnCj6G9s',
+      authDomain: 'ng-recipe-book-9b5fe.firebaseapp.com',
+    });
+  }
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -3,18 +3,27 @@ import {Injectable} from '@angular/core';
 
 import {RecipeService} from '../recipes/recipe.service';
 import {Recipe} from '../recipes/recipe.model';
+import {AuthService} from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
 
-  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+  constructor(private http: HttpClient,
+              private recipeService: RecipeService,
+              private authService: AuthService) {}
 
   storeRecipes() {
-    return this.http.put('https://ng-recipe-book-9b5fe.firebaseio.com/recipe.json', this.recipeService.getRecipes());
+
+    const token = this.authService.getToken();
+
+    return this.http.put('https://ng-recipe-book-9b5fe.firebaseio.com/recipe.json?auth=' + token, this.recipeService.getRecipes());
   }
 
   getRecipes() {
-    return this.http.get('https://ng-recipe-book-9b5fe.firebaseio.com/recipe.json')
+
+    const token = this.authService.getToken();
+
+    return this.http.get('https://ng-recipe-book-9b5fe.firebaseio.com/recipe.json?auth=' + token)
       .subscribe(
         (recipes: Recipe[]) => {
           this.recipeService.setRecipes(recipes);
